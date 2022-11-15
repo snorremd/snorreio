2f530841b254e9bd1906a9b7e3b99f69c6a53a39
Snorre Magnus Davøen <post@snorre.io>
2022-11-15 23:50:15 +0100

    Complete Astro layout and styling
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  plugins: [],
+  plugins: [
+    
+  ],
   theme: {
     fontFamily: {
       "nanum": ["nanum-pen-script"]
