742370da272553f9025fb3eae8fe24c8b261846d
Snorre Magnus Davøen <post@snorre.io>
2022-11-19 17:25:34 +0100

    Use astro-robots-txt to generate robots file
    To tell search engines they are allowed to crawl the site a robots.txt
    file should be present on the root path. The astro-robots-txt
    integration can automate generating this file.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import image from '@astrojs/image';
+import robotsTxt from 'astro-robots-txt';
 import rehypeInferDescriptionMeta from 'rehype-infer-description-meta';
 import { descriptionRemarkPlugin } from './src/plugins/description-remark-plugin.mjs';
 
@@ -12,6 +13,7 @@ export default defineConfig({
   site: 'https://snorre.io',
   integrations: [
     sitemap({}),
+    robotsTxt({}),
     tailwind(),
     mdx({
     rehypePlugins: [rehypeInferDescriptionMeta, descriptionRemarkPlugin]
