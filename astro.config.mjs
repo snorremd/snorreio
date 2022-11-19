644c8f960330fefff214b7448a4eb613f8dd1cf5
Snorre Magnus Davøen <post@snorre.io>
2022-11-19 17:46:39 +0100

    Add prefetching integration
    The prefetching integration enables prefetching of links. This allows
    the browser to start downloading resources as soon as possible.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,19 +7,16 @@ import robotsTxt from 'astro-robots-txt';
 import rehypeInferDescriptionMeta from 'rehype-infer-description-meta';
 import { descriptionRemarkPlugin } from './src/plugins/description-remark-plugin.mjs';
 
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://snorre.io',
-  integrations: [
-    sitemap({}),
-    robotsTxt({}),
-    tailwind(),
-    mdx({
+  integrations: [sitemap({}), robotsTxt({}), tailwind(), mdx({
     rehypePlugins: [rehypeInferDescriptionMeta, descriptionRemarkPlugin]
   }), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })],
+  }), prefetch()],
   markdown: {
     rehypePlugins: [rehypeInferDescriptionMeta, descriptionRemarkPlugin]
   }
