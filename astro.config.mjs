fc5a5c89a804e808423410c5d60dd9c06791b3b6
Snorre Magnus Davøen <post@snorre.io>
2022-11-19 17:22:16 +0100

    Add sitemap using official sitemap integration
    The astro sitemap integration automatically generates sitemap xml files
    for pages.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,22 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import image from '@astrojs/image';
-import rehypeInferDescriptionMeta from 'rehype-infer-description-meta'
-import { descriptionRemarkPlugin } from './src/plugins/description-remark-plugin.mjs'
+import rehypeInferDescriptionMeta from 'rehype-infer-description-meta';
+import { descriptionRemarkPlugin } from './src/plugins/description-remark-plugin.mjs';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://snorre.io',
   integrations: [
+    sitemap({}),
     tailwind(),
     mdx({
-      rehypePlugins: [rehypeInferDescriptionMeta, descriptionRemarkPlugin],
-    }),
-    sitemap({}),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    })],
+    rehypePlugins: [rehypeInferDescriptionMeta, descriptionRemarkPlugin]
+  }), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
   markdown: {
-    rehypePlugins: [rehypeInferDescriptionMeta, descriptionRemarkPlugin],
-  },
+    rehypePlugins: [rehypeInferDescriptionMeta, descriptionRemarkPlugin]
+  }
 });
