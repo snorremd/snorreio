57a283c976b3770e7b97207421a4b9b638c18bde
Snorre Magnus Davøen <snorre.magnus.davoen@adventuretech.no>
2023-08-31 08:29:47 +0200

    chore: Update to Astro 3.0 and use new image component
    Signed-off-by: Snorre Magnus Davøen <snorre.magnus.davoen@adventuretech.no>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from "@astrojs/mdx";
 import solid from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import robotsTxt from "astro-robots-txt";
 import rehypeInferDescriptionMeta from "rehype-infer-description-meta";
 import { descriptionRemarkPlugin } from "./src/plugins/description-remark-plugin.mjs";
@@ -33,15 +32,9 @@ export default defineConfig({
     mdx({
       rehypePlugins: [rehypeInferDescriptionMeta, descriptionRemarkPlugin],
     }),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     prefetch(),
   ],
   markdown: {
     rehypePlugins: [rehypeInferDescriptionMeta, descriptionRemarkPlugin],
-  },
-  experimental: {
-    viewTransitions: true
   }
 });
