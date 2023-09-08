b9164be6cd320008e558884a1fc76b0ab2ad8bf0
Snorre Magnus Davøen <post@snorre.io>
2023-09-08 18:57:06 +0200

    fix: Convert og images to png and design improvements (#43)
    * fix: Convert og images to png and design improvements
    
    The satori plugin now uses resvg, just like Vercel in its own service,
    to convert the Satori svg output to png. Fix #42.
    
    Some changes/simplifications were made to the og image layout. There is
    no need to show descriptions that will be listed by cards in proper
    text.
    
    * Use shortstack, remove unecessary fonts
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

--- src/plugins/satori-plugin.ts
+++ src/plugins/satori-plugin.ts
@@ -7,33 +7,31 @@ import satori from "satori";
 import { html } from "satori-html";
 import { readFile, writeFile } from "fs/promises";
 import { parse } from "node-html-parser";
-
+import { Resvg } from "@resvg/resvg-js"
 
 
 interface OgImageTemplateProps {
   site: string;
   title: string;
-  description: string;
   author: string;
+  profilePic: string;
+  wavesSvg: string;
 }
 
-function OgImageTemplate({ site, title, description, author }: OgImageTemplateProps) {
+function OgImageTemplate({ site, title, profilePic, wavesSvg }: OgImageTemplateProps) {
   return html`
     <div class="bg-stone-900 text-stone-200 w-full h-full flex flex-col justify-start">
       <div class="flex flex-col justify-center items-center" style="font-family: shortstack;">
-        <h2 class="text-2xl m-0 text-center pt-4 pb-4">
+        <h1 class="text-7xl m-0 text-center px-16 py-32">
           Snorre.io
-        </h2>
-        <h1 class="text-6xl m-0 text-center px-16">
-          ${title}
         </h1>
       </div>
-      <img src="https://snorre.io/graphics/layered-waves-dark.svg" class="w-full h-32" />
-      <div class="flex flex-col bg-stone-800 pb-16 px-16 grow justify-center" style="font-family: ubuntu;">
-        <div class="flex flex-row max-w-full items-center -mt-8"> 
-          <img src="https://ca.slack-edge.com/T9EJR0Z46-US8V5V5E3-b099507917a1-512" class="w-32 h-32 rounded-lg mr-8" />
-          <p class="text-4xl font-ubuntu m-0">
-            ${description}
+      <img src="data:image/svg+xml;base64,${wavesSvg}" class="w-full h-32 -mt-24" />
+      <div class="flex flex-col bg-stone-800 pb-16 px-32 grow justify-center" style="font-family: shortstack;">
+        <div class="flex flex-row max-w-full items-center"> 
+          <img src="data:image/jpeg;base64,${profilePic}" class="w-32 h-32 rounded-lg mr-8" />
+          <p class="text-4xl m-0">
+            ${title}
           </p>
         </div>
       </div>
@@ -44,12 +42,14 @@ function OgImageTemplate({ site, title, description, author }: OgImageTemplatePr
 interface GenerateOgImageProps {
   site: string;
   route: RouteData;
+  profilePic: string;
+  wavesSvg: string;
 }
 
 /**
  * Generate an og:image for a given route
  */
-async function generateOgImage({ site, route }: GenerateOgImageProps) {
+async function generateOgImage({ site, route, profilePic, wavesSvg }: GenerateOgImageProps) {
   // First we get the html for the route and parse out the title and description
   const html = await readFile(route.distURL!, { encoding: "utf-8" });
   const root = parse(html.toString());
@@ -60,7 +60,8 @@ async function generateOgImage({ site, route }: GenerateOgImageProps) {
   const author = "Snorre Magnus Davøen";
 
   // Then we generate the URL for the og:image
-  const template = OgImageTemplate({ site, title, description, author });
+  const template = OgImageTemplate({ site, title, author, profilePic, wavesSvg });
+
 
   try {
     const svg = await satori(template, {
@@ -73,21 +74,20 @@ async function generateOgImage({ site, route }: GenerateOgImageProps) {
           data: await readFile("./public/fonts/shortstack-regular.woff"),
           style: "normal",
         },
-        {
-          // Ubuntu font for bread text in satori
-          name: "ubuntu",
-          data: await readFile("./public/fonts/ubuntu-regular.ttf"),
-          style: "normal",
-        }
       ],
     });
 
     // calculate write path by replacing index.html with index-og.svg
     const writePath = route.distURL!.pathname.replace(
       "index.html",
-      "index-og.svg",
+      "index-og.png",
     );
-    await writeFile(writePath, svg);
+
+    // Convert svg to png 
+    const resvg = new Resvg(svg, {})
+    const png = await resvg.render()
+    const pngBuffer = await png.asPng()
+    await writeFile(writePath, pngBuffer);
   } catch (e) {
     console.error("Could not generate og:image for route", route.route, e);
   }
@@ -107,10 +107,15 @@ export function satoriPlugin(): AstroIntegration {
       },
       "astro:build:done": async ({ routes }) => {
         console.log("Build done, generate og:image for each route");
+
+        const profilePic = (await readFile("./public/images/profile.jpeg")).toString("base64");
+        const wavesSvg = (await readFile("./public/graphics/layered-waves-dark.svg")).toString("base64");
+        
+
         const ogImages = routes
           .filter((route) => route.distURL?.pathname?.endsWith("index.html")?? false)
           .map((route) =>
-            generateOgImage({ route, site: astroConfig.site ?? "" }),
+            generateOgImage({ route, site: astroConfig.site ?? "", profilePic, wavesSvg }),
           );
 
         await Promise.all(ogImages);
