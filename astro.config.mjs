6d0e8a153eebb7190c3834155270b712063bc564
Snorre Magnus Davøen <snorre.magnus.davoen@adventuretech.no>
2024-04-23 19:04:56 +0200

    chore: Update deps, cleanup config, fix <title>
    The <title> tag was being rendered incorrectly apparently due to Astro's
    <ViewTransitions /> element being embedded in the BaseHead component.
    Super weird! Moved the <ViewTransitions /> component out into the
    BaseLayout component instead.
    
    Cleaned up Astro config as a lot of the configuration seemed to be
    deprecated and no longer needed as things are more built into Astro.
    
    Update dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import solid from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-import rehypeInferDescriptionMeta from "rehype-infer-description-meta";
-import { descriptionRemarkPlugin } from "./src/plugins/description-remark-plugin.mjs";
-import prefetch from "@astrojs/prefetch";
 import analyze from "rollup-plugin-analyzer";
 import { visualizer } from "rollup-plugin-visualizer";
 import devtools from "solid-devtools/vite";
@@ -26,17 +23,14 @@ export default defineConfig({
       exclude: ["@resvg/resvg-js"]
     }
   },
-  integrations: [icon({
+  integrations: [mdx(), icon({
     include: {
       // Include only three `mdi` icons in the bundle
       mdi: ["twitter", "github", "link-variant", "download"],
       "material-symbols": ["download", "copyright-outline"],
       "fa": ["soundcloud"]
     }
-  }), satoriPlugin(), sitemap({}), robotsTxt({}), tailwind(), solid(), mdx({
-    rehypePlugins: [rehypeInferDescriptionMeta, descriptionRemarkPlugin]
-  }), prefetch()],
-  markdown: {
-    rehypePlugins: [rehypeInferDescriptionMeta, descriptionRemarkPlugin]
-  }
+  }), satoriPlugin(), sitemap({}), robotsTxt({}), tailwind(), solid(), ],
+
+  prefetch: true
 });
