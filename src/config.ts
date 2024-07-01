32d597e83b65e99648df02571dbe461ffac74db6
Snorre Magnus Davøen <snorre.magnus.davoen@adventuretech.no>
2024-07-01 15:24:45 +0200

    chore: Update site title and description
    In order to boost search result hits for frontend developer and Bergen
    searches, use a more descriptive site title and description.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Snorre.io";
+export const SITE_TITLE = "Snorre Davøen | Frontend Developer | Bergen";
 export const SITE_DESCRIPTION =
-  "Snorre.io is a blog about web development, programming, and other things.";
+  "Snorre.io is the personal blog of Snorre Magnus Davøen, a fullstack developer from Bergen, Norway specializing in frontend. I write about web development, programming, and other things.";
