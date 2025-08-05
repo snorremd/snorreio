39578902caf4d4ba8a65c7190bdaf07d4fbc952c
Snorre Magnus Davøen <post@snorre.io>
2025-08-05 23:23:00 +0200

    chore: Tailwind 4 with new setup (#82)
    * chore: Tailwind 4 with new setup
    
    * Fix formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import solid from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 import devtools from "solid-devtools/vite";
 import icon from "astro-icon";
 import { satoriPlugin } from "./src/plugins/satori-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from "@astrojs/cloudflare";
 
@@ -19,6 +19,7 @@ export default defineConfig({
         /* features options - all disabled by default */
         autoname: true, // e.g. enable autoname
       }),
+      tailwindcss(),
     ],
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
@@ -45,7 +46,6 @@ export default defineConfig({
     satoriPlugin(),
     sitemap({}),
     robotsTxt({}),
-    tailwind(),
     solid(),
   ],
   prefetch: true,
